61d2bddc94c8da1b715c46cb4dac5f782274db3b
hauss <philhaussler@gmail.com>
2013-04-04 16:38:23 -0500

    Initial Commit

---
+++ ideaboxx/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideaboxx::Application.config.secret_token = 'da58750ef76e0999c9b7c4d1738370184c45b226ab6390b8e190a994263b9c91bf8054fa021a9ed55cc3859df095d29c71a5de8655df3533a21b44868b285c58'
