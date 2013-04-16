40df25fbec2249793b6848e5ca17cae5211e0401
hauss <philhaussler@gmail.com>
2013-04-16 13:47:59 -0500

    Add Last 5 Ideas to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def home
+  	@recentIdeas = Idea.order("created_at").last(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @ideas }
+    end
   end
 
   def about
