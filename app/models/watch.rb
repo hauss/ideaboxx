8bab25141fdec64f3822db131edd521bc3cc4457
hauss <philhaussler@gmail.com>
2013-05-02 12:45:32 -0500

    Show only user name in the Idea Watchers list and add a link --though not yet functional -- to add or remove yourself from Watchers list

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -3,4 +3,5 @@ class Watch < ActiveRecord::Base
 
   belongs_to :watcher, :class_name => 'User', :foreign_key => "user_id"
 	belongs_to :watched_idea, :class_name => 'Idea', :foreign_key => "idea_id"
+
 end
